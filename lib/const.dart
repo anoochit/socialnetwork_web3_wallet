e8ab5f1a0e6e96e4e1d1b54156b6ba30d2a0c325
Anuchit Chalothorn <anoochit@gmail.com>
2022-05-10 19:09:58 +0700

    update const

--- lib/const.dart
+++ lib/const.dart
@@ -1,7 +1,4 @@
 // rpc server
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:snwallet/models/user.dart';
-
 const String rpcUrl = "http://10.0.2.2:7545";
 const String wsUrl = 'ws://10.0.2.2:8545';
 const String blockExplorer = 'http://10.0.2.2:4000';
